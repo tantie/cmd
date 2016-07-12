96ec9973a897a45ed2a90dfa7c37804882ad1edf
tantie <mrcr@pro-2.local>
2016-07-12 16:41:20 +0300

    run chome full screan
    Запускаем chrome на весь экран

--- multi_fullscreen.cmd
+++
@@ -1,3 +0,0 @@
-"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --user-data-dir=c:/screen1 --start-fullscreen --new-window www.domain.com --new-window "%1" --window-position=0,0
-
-"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --user-data-dir=c:/screen2 --start-fullscreen --new-window www.domain2.com --new-window "%2" --window-position=1680,0
